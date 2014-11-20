35e4cd04003d70cb198278502e8e988501815f0c
msmykowski <smycal@gmail.com>
2014-11-20 15:39:07 -0800

    add first revisions

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,8 @@
 class WelcomeController<ApplicationController
   def home
-    @quotes_array=["Failure is not an option. Everyone has to Succeed",
-      "Your time is limited so don't waste it living someone elses life",
-      "Better ingredients, better pizza",]
+    @quotes_array=["\"Failure is not an option. Everyone has to Succeed\"",
+      "\"Your time is limited so don't waste it living someone elses life\"",
+      "\"Better ingredients, better pizza\"",]
 
     @quote_authors=["Arnold Schwarzenegger","Steve Jobs","Papa Johns"]
   end
