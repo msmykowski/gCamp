3d29bfa36da3b95dc5655d04838645894979b0be
msmykowski <smycal@gmail.com>
2014-11-20 14:33:07 -0800

    general updates

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,7 @@ class WelcomeController<ApplicationController
     @quotes_array=["Failure is not an option. Everyone has to Succeed",
       "Your time is limited so don't waste it living someone elses life",
       "Better ingredients, better pizza",]
+
     @quote_authors=["Arnold Schwarzenegger","Steve Jobs","Papa Johns"]
   end
 
