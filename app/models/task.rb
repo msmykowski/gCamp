8367acdd9ab4740f214f3aabe2589e043ab52cbf
msmykowski <smycal@gmail.com>
2014-12-15 19:46:20 -0800

    add validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,13 @@
 class Task < ActiveRecord::Base
 
-  validates :description, presence: true 
+  validates :description, presence: true
+
+  def due_date_cannot_be_in_the_past
+    if due_date < Date.today
+      errors.add(:due_date, "can't be in the past")
+    end
+  end
+
+  validate :due_date_cannot_be_in_the_past, on: :create
 
 end
---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+
+  validates :first_name, :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+
+end
